10b85b6ab919653485f2bcc449105a3fee0668ac
Javier Jarp <javi.jarp1@gmail.com>
2024-01-17 19:50:36 -0400

    Pgit

--- Ejercicios1/Problema 10/Program.cs
+++ Ejercicios1/Problema 10/Program.cs
@@ -26,6 +26,22 @@ while (!int.TryParse(string_num2, out int intValue)){
 if (Convert.ToInt32(string_num1) - Convert.ToInt32(string_num2) <= 10){
     Console.WriteLine("La diferencia entre " + string_num1 + " y " + string_num2 + " es menor o igual que 10.");
 
-    
 
+    Console.WriteLine(string.Format("Todos los digitos dentro del numero {0}.", string_num1));
+    if (string_num1[0].ToString() == "-") {
+        string_num1 = string_num1.Substring(1);
+    }
+    foreach (char i in string_num1)
+    {
+        Console.WriteLine(i);
+    }
+
+    Console.WriteLine(string.Format("Todos los digitos dentro del numero {0}.", string_num2));
+    if (string_num2[0].ToString() == "-") {
+        string_num2 = string_num2.Substring(1);
+    }
+    foreach (char i in string_num2)
+    {
+        Console.WriteLine(i);
+    }
 }
---
+++ En Clase/Funciones/Ejercicio En Clase Funciones/Program.cs
@@ -0,0 +1,45 @@
+﻿void Print(string input){
+    Console.WriteLine(input);
+}
+
+string Read(){
+    return Console.ReadLine();
+}
+
+int Sumar(int num1, int num2){
+    return num1 + num2;
+}
+
+int Restar(int num1, int num2){
+    return num1 - num2;
+}
+
+int Multiplicar(int num1, int num2){
+    return num1 * num2;
+}
+
+int Dividir(int num1, int num2){
+    return num1 / num2;
+}
+
+
+Print("Ingrese un numero: ");
+string string_num1 = Read();
+while (!int.TryParse(string_num1, out int intValue)) {
+    Print("Eso no es un numero.");
+    Print("Intentalo de nuevo.");
+    string_num1 = Read();
+}
+Print("Ingrese otro numero: ");
+string string_num2 = Read();
+
+while (!int.TryParse(string_num2, out int intValue)) {
+    Print("Eso no es un numero.");
+    Print("Intentalo de nuevo.");
+    string_num2 = Read();
+}
+
+int num1 = int.Parse(string_num1);
+int num2 = int.Parse(string_num2);
+
+Print(String.Format("La suma de ambos numeros es: {0}. \n La resta de ambos numeros es: {1}. \n El producto de ambos numeros es: {2}. \n El cociente de ambos numeros es: {3}.", Sumar(num1, num2).ToString(), Restar(num1, num2).ToString(), Multiplicar(num1, num2).ToString(), num1, num2.ToString()));
