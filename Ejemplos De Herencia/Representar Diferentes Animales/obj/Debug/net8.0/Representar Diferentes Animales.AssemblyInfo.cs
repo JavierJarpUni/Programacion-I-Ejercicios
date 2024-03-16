3a6dd12522b47bcc53b30408018cacb0da022ffe
Javier Jarp <Javi.jarp1@gmail.com>
2024-03-16 13:36:40 -0400

    Merging with the desktop

--- Ejemplos De Herencia/Representar Diferentes Animales/obj/Debug/net8.0/Representar Diferentes Animales.AssemblyInfo.cs
+++ Ejemplos De Herencia/Representar Diferentes Animales/obj/Debug/net8.0/Representar Diferentes Animales.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Representar Diferentes Animales")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06396cafe153c9214b0e75f872cee0de4cede154")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9102cc01273feb7b3c81a969fa1ed64dbb61313c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Representar Diferentes Animales")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Representar Diferentes Animales")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
